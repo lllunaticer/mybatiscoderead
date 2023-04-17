a52d115cdd17812a27393968d9b78e8d6c7470b1
longxingjian <longxingjian@kuaishou.com>
2023-04-17 17:03:50 +0800

    add some configuration

--- src/main/java/com/mybatis/coderead/mybatiscoderead/MainController.java
+++ src/main/java/com/mybatis/coderead/mybatiscoderead/MainController.java
@@ -1,22 +1,19 @@
 package com.mybatis.coderead.mybatiscoderead;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 @RequestMapping("/")
 public class MainController {
-    @Autowired
+//    @Autowired
     private UserMapper userMapper;
 
     @RequestMapping("/")
     public Object index(){
         User userParam = new User();
         userParam.setSchoolName("tju");
-        List<User> users = userMapper.queryUserBySchoolName(userParam);
-        return users;
+        return userMapper.queryUserBySchoolName(userParam);
     }
 }
